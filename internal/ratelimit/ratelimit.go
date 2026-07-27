77fbe84fba1b0b4f9707d55f201b4ffbc2317b6a
elgeorgiou <1Eleanaki@gmail.com>
2026-07-27 22:53:54 +0300

    feat(ratelimit): implement throttled reader

--- internal/ratelimit/ratelimit.go
+++ internal/ratelimit/ratelimit.go
@@ -2,9 +2,29 @@ package ratelimit
 
 import (
 	"io"
+	"time"
 )
 
-func NewReader(r io.Reader, bytesPerSec int64) io.Reader {
+type reader struct{
+	r 			io.Reader
+	bytesPerSec int64
+}
 
-	return r
+func NewReader(r io.Reader, bytesPerSec int64) io.Reader {
+	
+	return &reader{
+		r:			 r,
+		bytesPerSec: bytesPerSec,
+	} 
+}
+
+func (r *reader) Read(p []byte) (int, error) {
+
+	n, err := r.r.Read(p)
+
+	if n > 0 && r.bytesPerSec > 0 {
+	time.Sleep(time.Duration(int64(n)) * time.Second / time.Duration(r.bytesPerSec))
+	}
+
+	return n, err
 }
