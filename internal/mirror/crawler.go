91f79a42fac6fc9abdcfdd6a559af5b799c0e683
elgeorgiou <1Eleanaki@gmail.com>
2026-08-16 15:40:31 +0300

    feat(crawler): implement basic crawl loop with visited map

--- internal/mirror/crawler.go
+++ internal/mirror/crawler.go
@@ -1,5 +1,57 @@
-package mirror 
+package mirror
 
-func Mirror(rootURL string, outputDir string) error{
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	downloader "wgetclone/internal/downloader"
+)
+
+type MirrorOptions struct {
+	RejectSuffixes []string
+	ExcludePaths   []string
+	ConvertLinks   bool
+}
+
+func Mirror(
+	rootURL string,
+	opts MirrorOptions,
+	dl func(downloader.DownloadConfig) (string, error),
+) error {
+	pendingURLs := []string{rootURL}
+	visitedURLs := make(map[string]bool)
+
+	for len(pendingURLs) > 0 {
+		currentURL := pendingURLs[0]
+		pendingURLs = pendingURLs[1:]
+
+		parsedURL, err := url.Parse(currentURL)
+		if err != nil {
+			return fmt.Errorf("failed to parse URL %q: %w", currentURL, err)
+		}
+
+		parsedURL.Fragment = ""
+
+		if parsedURL.Path != "/" {
+			parsedURL.Path = strings.TrimRight(parsedURL.Path, "/")
+		}
+
+		currentURL = parsedURL.String()
+
+		if visitedURLs[currentURL] {
+			continue
+		}
+
+		visitedURLs[currentURL] = true
+
+		_, err = dl(downloader.DownloadConfig{
+			URL: currentURL,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to download URL %q: %w", currentURL, err)
+		}
+	}
+
 	return nil
 }
